ea073f6c48c7627c044d062913d5f083d6773ed6
ringolove <mybloodyvalentine@outlook.kr>
2022-01-03 14:58:38 +0900

    circle revisited

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 window.onload = function() {
     var circle = document.getElementById("circle");
-    alert(circle);
-   }
+    // alert(circle);
+    setTimeout(function(){
+            circle.style.marginLeft = "800px";
+            circle.style.transition = "4s";
+            circle.style.transform = "rotate(360deg)";
+    },2000);
+    setTimeout(function(){
+        circle.style.marginLeft = "50px";
+        circle.style.transition = "4s";
+        circle.style.transform = "rotate(360deg)";
+    },4000);
+}
