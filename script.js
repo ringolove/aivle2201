bcbfa0c8c36240e9f2ab6bc548177c89cd08ee44
ringolove <mybloodyvalentine@outlook.kr>
2022-01-03 14:23:16 +0900

    alert circle

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 window.onload = function() {
     var circle = document.getElementById("circle");
-    setTimeout(function() {
-    circle.style.marginLeft = "800px";
-    circle.style.transition = "4s";
-    circle.style.transform = "rotate(360deg)";}, 2000);
+    alert(circle);
    }
